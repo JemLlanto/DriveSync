5fc127ec916981133d465ed896fc6238a0ccad7c
JemLlanto <105899347+JemLlanto@users.noreply.github.com>
2026-08-13 17:10:30 +0800

    resetTripMeter function moved to data cards component

--- app/vehicle/DataCards.tsx
+++ app/vehicle/DataCards.tsx
@@ -1,118 +1,148 @@
 import Button from "@/components/Button";
 import { ThemeColors, useTheme } from "@/lib/theme";
-import { Vehicle } from "@/lib/vehicleStore";
+import { useVehicles, Vehicle } from "@/lib/vehicleStore";
 import { formatNumber, roundUp2 } from "@/utils/formatting";
 import { Ionicons } from "@expo/vector-icons";
 import { Dispatch, SetStateAction, useMemo, useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 import HistoryModal from "./HistoryModal";
 
 interface DataCardsProps {
   vehicle: Vehicle;
+  setVehicle: Dispatch<SetStateAction<Vehicle>>;
   setModalVisible: Dispatch<SetStateAction<boolean>>;
   setFullTankMethod: Dispatch<SetStateAction<boolean>>;
-  resetTripMeter: () => void;
 }
 
 export default function DataCards({
   vehicle,
+  setVehicle,
   setModalVisible,
   setFullTankMethod,
-  resetTripMeter,
 }: DataCardsProps) {
   const { colors } = useTheme();
   const styles = useMemo(() => createStyles(colors), [colors]);
+  const { resetTripMeterOdo, loading } = useVehicles();
   const [historyVisible, setHistoryVisible] = useState<boolean>(false);
+
+  const resetTripMeter = () => {
+    vehicle.tripOdo === 0 || !vehicle.tripOdo
+      ? Alert.alert(
+          "Reset Trip Meter",
+          `You can't reset trip meter for "${vehicle.name}" because its currently 0.`,
+          [{ text: "Close", style: "cancel" }],
+        )
+      : Alert.alert(
+          "Reset Trip Meter",
+          `Reset the trip meter for "${vehicle.name}"?`,
+          [
+            { text: "Cancel", style: "cancel" },
+            {
+              text: "Reset",
+              style: "destructive",
+              onPress: async () => {
+                const response = await resetTripMeterOdo(vehicle.id);
+                if (response.success) {
+                  // Reset trip odo to zero
+                  setVehicle((prev) => ({ ...prev, tripOdo: 0 }));
+                }
+              },
+            },
+          ],
+        );
+  };
 
   return (
     <>
-      {/* ODO READING */}
-      <View style={styles.odoCard}>
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            margin: 15,
-          }}
-        >
-          {/* HISTORY BUTTON */}
-          <Pressable
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setHistoryVisible(true)}
-          >
-            <Ionicons name="time-outline" size={25} color={colors.text} />
-          </Pressable>
-        </View>
-        <Text style={styles.odoLabel}>Current Odometer</Text>
-        <Text style={styles.odoValue}>{formatNumber(vehicle.odo)}</Text>
-        <Text style={styles.odoUnit}>kilometers</Text>
-        <Button
-          icon="speedometer-outline"
-          buttonText="Update Odometer"
-          onPress={() => setModalVisible(true)}
-        />
-      </View>
-      {/* TRIP ODO & GAS CONSUMPTION */}
-      <View style={styles.TripGasCard}>
-        {/* TRIP CARD */}
-        <View
-          style={{
-            flex: 1,
-            flexShrink: 1,
-            alignItems: "center",
-          }}
-        >
-          <Text style={styles.odoLabel}>Current Trip</Text>
-          <Text style={styles.GasConsValue}>
-            {roundUp2(vehicle.tripOdo || 0)} km
-          </Text>
+      <View>
+        {/* ODO READING */}
+        <View style={styles.odoCard}>
           <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 10,
+              position: "absolute",
+              top: 0,
+              right: 0,
+              margin: 15,
             }}
           >
-            <Button
-              variant="secondary"
-              icon="refresh-outline"
-              buttonText="Reset"
-              onPress={resetTripMeter}
-            />
-          </View>
-        </View>
-        {/* GAS CONSUMPTION CARD */}
-        <View
-          style={{
-            flex: 1,
-            flexShrink: 1,
-            alignItems: "center",
-          }}
-        >
-          <Text style={styles.odoLabel}>Gas Consumption</Text>
-          <Text style={styles.GasConsValue}>
-            {roundUp2(vehicle.gasConsumption || 0)} km/ltr
-          </Text>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 10,
-            }}
-          >
-            <Button
-              variant="secondary"
-              icon="calculator-outline"
-              buttonText="Calculate"
-              onPress={() => {
-                setModalVisible(true);
-                setFullTankMethod(true);
+            {/* HISTORY BUTTON */}
+            <Pressable
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
               }}
-            />
+              onPress={() => setHistoryVisible(true)}
+            >
+              <Ionicons name="time-outline" size={25} color={colors.text} />
+            </Pressable>
+          </View>
+          <Text style={styles.odoLabel}>Current Odometer</Text>
+          <Text style={styles.odoValue}>{formatNumber(vehicle.odo)}</Text>
+          <Text style={styles.odoUnit}>kilometers</Text>
+          <Button
+            icon="speedometer-outline"
+            buttonText="Update Odometer"
+            onPress={() => setModalVisible(true)}
+          />
+        </View>
+        {/* TRIP ODO & GAS CONSUMPTION */}
+        <View style={styles.TripGasCard}>
+          {/* TRIP CARD */}
+          <View
+            style={{
+              flex: 1,
+              flexShrink: 1,
+              alignItems: "center",
+            }}
+          >
+            <Text style={styles.odoLabel}>Current Trip</Text>
+            <Text style={styles.GasConsValue}>
+              {roundUp2(vehicle.tripOdo || 0)} km
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 10,
+              }}
+            >
+              <Button
+                variant="secondary"
+                icon="refresh-outline"
+                buttonText="Reset"
+                onPress={resetTripMeter}
+              />
+            </View>
+          </View>
+          {/* GAS CONSUMPTION CARD */}
+          <View
+            style={{
+              flex: 1,
+              flexShrink: 1,
+              alignItems: "center",
+            }}
+          >
+            <Text style={styles.odoLabel}>Gas Consumption</Text>
+            <Text style={styles.GasConsValue}>
+              {roundUp2(vehicle.gasConsumption || 0)} km/ltr
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 10,
+              }}
+            >
+              <Button
+                variant="secondary"
+                icon="calculator-outline"
+                buttonText="Calculate"
+                onPress={() => {
+                  setModalVisible(true);
+                  setFullTankMethod(true);
+                }}
+              />
+            </View>
           </View>
         </View>
       </View>
